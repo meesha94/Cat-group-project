858c3921a335446e4a8c64aacd769d7a03cf8d73
Lapinas <mrlapinas99@gmail.com>
2022-01-26 12:10:58 +0000

    Add cats to the basket

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './index.css';
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [cat, setCat] = useState({})
+  // For the Basket
+  const [basket, setBasket] = useState(0);
+ 
+
+  // ADD to the Basket
+  const addBasket = () => {
+    setBasket(basket + 1)
+  };
+
+  // REMOVE from the Basket
+  const removeBasket = () => {
+    setBasket(basket - 1)
+    if (basket == 0 ) {
+        setBasket(0)
+    }
+  };
+
+
+  useEffect(() => {
+    handleFetch()
+  }, [])
+
+  const handleFetch = async () => {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=5")
+    const data = await response.json()
+    console.log(data)
+
+    setCat(data)
+  };
+
+  return (
+    <div>
+      <h1>The Cat Shop </h1>
+      <h1>In the Basket: {basket}</h1>
+      
+      {cat.length > 0 && cat.map((item, index) => {
+        return (
+          <div>
+            <button onClickCapture={addBasket}>ADD</button>
+            <button onClick={removeBasket}>REMOVE</button>
+            <p>{item.text}</p>
+            <img src={item.url} alt="cat" />
+          </div>
+        )
+      })}
+
+
+    </div>
+  )
+};
+
+export default App;
+
