4a2673f0055e279f5388c085f5243520e63720f1
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-01-26 16:57:41 +0000

    styling for cat images

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import './App.css';
+import './index.css';
 import { useState, useEffect } from "react";
-
-
 
 const App = () => {
   const [cat, setCat] = useState({})
- 
+  // For the Basket
+  const [basket, setBasket] = useState(0);
+
+
+  // ADD to the Basket
+  const addBasket = () => {
+    setBasket(basket + 1)
+    if (basket == 5) {
+      setBasket(5)
+    }
+  };
+
+  // REMOVE from the Basket
+  const removeBasket = () => {
+    setBasket(basket - 1)
+    if (basket == 0) {
+      setBasket(0)
+    }
+  };
+
 
   useEffect(() => {
     handleFetch()
   }, [])
 
   const handleFetch = async () => {
-    const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=5")
+    const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
     const data = await response.json()
     console.log(data)
 
@@ -22,19 +39,28 @@ const App = () => {
   return (
     <div>
       <h1>The Cat Shop </h1>
-      
-      {cat.length > 0 && cat.map((item, index) => {
-        return (
-          <div>
-            <p>{item.text}</p>
-            <img src={item.url} alt="cat" />
-          </div>
-        )
-      })}
+      <h4>In the Basket: {basket}</h4>
+      <div className="grid">
+        {cat.length > 0 && cat.map((item, index) => {
+
+          return (
+            <div className="item">
+              <p >{item.text}</p>
+              <img src={item.url} alt="cat" />
+              <br />
+              <button onClickCapture={addBasket}>ADD</button>
+              <button onClick={removeBasket}>REMOVE</button>
+            </div>
+
+          )
+
+        })}
+      </div>
 
     </div>
   )
 };
 
 export default App;
+
 
