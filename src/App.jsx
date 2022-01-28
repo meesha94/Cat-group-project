14934b7b0f9fc1597679b314c3191a7005918bd7
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-01-28 10:30:37 +0000

    added productinfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './index.css';
 import { useState, useEffect } from "react";
-import { name } from "faker";
+import { faker } from '@faker-js/faker';
 
 
 const App = () => {
@@ -11,22 +11,19 @@ const App = () => {
   // ADD to the Basket
   const addBasket = () => {
     setBasket(basket + 1)
-    if (basket == 5) {
-      setBasket(5)
+    if (basket === 10) {
+      setBasket(10)
     }
   };
 
   // REMOVE from the Basket
   const removeBasket = () => {
     setBasket(basket - 1)
-    if (basket == 0) {
+    if (basket === 0) {
       setBasket(0)
     }
   };
   
-  
-  
-  let firstName = name.firstName();
   
   useEffect(() => {
     handleFetch()
@@ -53,7 +50,10 @@ const App = () => {
             <div className="item">
               <p >{item.text}</p>
               <img src={item.url} alt="cat" />
-              <p>{name.firstName()}</p>
+              <p>{faker.name.findName()}</p>
+              <p>£ {faker.datatype.number({min: 300,
+    max: 700})}</p>
+              <p>Please contact {faker.name.firstName()} by phone on {faker.phone.phoneNumber()} or by email {faker.internet.email()} for more information. </p>
               <br />
               <button onClickCapture={addBasket}>ADD</button>
               <button onClick={removeBasket}>REMOVE</button>
